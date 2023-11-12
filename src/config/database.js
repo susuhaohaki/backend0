68bbd2471fc73a086d759856606e727b220435ea
Hai Nguyen <nguyenhai250203@gmail.com>
2023-11-12 21:30:51 +0700

    31. Tái Sử Dụng Connection

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+// get the client
+require('dotenv').config();
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port:process.env.DB_PORT, //default 3306
+    user: process.env.DB_USER, //default : emty
+    password:process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const express = require('express'); //commonjs
 const app = express();//app express
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-// get the client
-const mysql = require('mysql2');
+const connection = require('./config/database')
 
 const port = process.env.PORT || 8888 //port --> hardcode
 const hostname = process.env.HOST_NAME
@@ -17,21 +16,13 @@ configViewEngine(app);
 app.use('/',webRoutes)
 
 //TEST CONNECTION
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port:3307, //default 3306
-  user: 'root', //default : emty
-  password:'123456',
-  database: 'hoidanit'
-});
+
 
 // simple query
 connection.query(
   'SELECT * from USERS',
   function(err, results, fields) {
     console.log('>>>>>RESULTS =',results); // results contains rows returned by server
-    console.log('>>>>> FIELD = ',fields); // fields contains extra meta data about results, if available
   }
 );
 

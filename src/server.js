33093cb147d154dd62191672724ceefcf2a5f785
susuhaohaki <nguyenhai250203@gmail.gmailcom>
2024-02-02 15:20:26 +0700

    #104. MongoDB Driver

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 const apiRoutes = require('./routes/api')
 const fileUpload = require('express-fileupload');
+const { MongoClient } = require('mongodb');
 
 
 const connection = require('./config/database')
@@ -33,7 +34,24 @@ app.use('/v1/api',apiRoutes);
 //TEST CONNECTION
   (async()=>{
     try {
-      await connection();
+      //using mongoose
+      // await connection();
+
+      //using mongodb driver
+      const url = process.env.DB_HOST_WITH_DRIVER;
+      const client = new MongoClient(url);
+      // Database Name
+      const dbName = process.env.DB_NAME;
+      
+      // Use connect method to connect to the server
+      await client.connect();
+      console.log('Connected successfully to server');
+      
+      const db = client.db(dbName);
+      const collection = db.collection('documents');
+      // the following code examples can be pasted here...
+      
+      //
       app.listen(port, hostname, () => {
         console.log(`backen zero app listening on port ${port}`)
       })
